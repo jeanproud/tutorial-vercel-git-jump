704a17d0a88abeed83e2e1de741c5d090f154b8b
Pedro Fappi <pedro.fappi@gmail.com>
2023-08-11 11:29:12 -0300

    0.0.8

--- api/static/script.js
+++ api/static/script.js
@@ -1,12 +1,16 @@
-modalImg = document.getElementById("modalImg")
-modal = document.getElementById("modal")
+
 function engorgio(el){
+    modal = document.getElementById("modal")
     modal.style.zIndex=100
+    modal.classList.toggle("collapsed")
+    modalImg = document.getElementById("modalImg")
     modalImg.src=el.src
     modalImg.classList.toggle("collapsed")
 }
 
 function reducio(el){
+    el.classList.toggle("collapsed");
+    modalImg = document.getElementById("modalImg")
     modalImg.classList.toggle("collapsed")
-    el.style.zIndex=0
+    setTimeout(() => { el.style.zIndex=0; }, 250);
 }
